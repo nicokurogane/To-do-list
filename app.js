957258d4ccddf185c6d3d9c6fd35983501146287
nicokurogane <fyedream@gmail.com>
2019-06-26 16:35:55 -0600

    added localStorage functionality. Now the data is persisted.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var arrayTask = [];
 var currentTaskId = 1;
 const form = document.getElementById("task-form");
 const taskTable = document.getElementById("task-table");
+const TASK_ARRAY_KEY = "task";
 
 
 //inicializamos la pagina con los datos ya cargados
@@ -9,7 +10,13 @@ window.addEventListener('load', function (event) {
     console.log('Aqui vamos a cargar el ID');
     //TODO: que este ID sea asignado dinamicamente
     document.getElementById('id').value = currentTaskId;
-    //TODO CARGAR EL ARRAY DEL LOCASTORE CUANDO SE CARGUE LA PAGINA
+
+    if (localStorage.getItem(TASK_ARRAY_KEY) !== null) {
+        arrayTask = JSON.parse(localStorage.getItem(TASK_ARRAY_KEY));
+        arrayTask.forEach(task => {
+            addTaskToTable(task);
+        });
+    }
 });
 
 
@@ -18,32 +25,31 @@ form.addEventListener('submit', handleSubmit);
 //interceptamos el el submit para extraer la data
 function handleSubmit(e) {
     e.preventDefault();
-    if(validateForm()) addTask();
+    if (validateForm()) {
+        addTask();
+    }
 }
 
 //devuelve false si el formulario no esta validado
 function validateForm() {
-
     let inputTaskValue = String(document.getElementById('task').value);
-    if( inputTaskValue.length > 100 || inputTaskValue === '' ){
+    if (inputTaskValue.length > 100 || inputTaskValue === '') {
         alert('task is not correct.');
         return false;
     }
 
     let statusInputsArray = Array.from(document.getElementsByName('status'));
     let numberOfChecked = 0;
-    statusInputsArray.forEach( checkbox => {
-        numberOfChecked += checkbox.checked ?  1:0;
+    statusInputsArray.forEach(checkbox => {
+        numberOfChecked += checkbox.checked ? 1 : 0;
     });
 
-    if(numberOfChecked === 0){
-        alert('you must select Status condition.');
-        return false;
-    } else if(numberOfChecked > 1){
-        alert('only 1 Status condition is allowed.');
+    if (numberOfChecked !== 1) {
+        alert('you must select 1 status condition.');
         return false;
     }
 
+    return true;
 }
 
 function addTask() {
@@ -58,11 +64,12 @@ function addTask() {
     });
     taskObject.creationDate = Date.now();
     arrayTask.push(taskObject);
+    addTaskToLocalStore();
     addTaskToTable(taskObject);
 }
 
 function addTaskToTable(taskToAdd) {
-    console.log(taskTable);    
+    console.log(taskTable);
     let newRow = `  <th scope="row">${taskToAdd.id}</th>
                     <td>${taskToAdd.task}</td>
                     <td>${taskToAdd.asignee}</td>
@@ -72,7 +79,7 @@ function addTaskToTable(taskToAdd) {
                       <button type="button" onclick="deleteTask(${taskToAdd.id})"> X </button>
                     </td>`;
     const tr = document.createElement('tr');
-    tr.id =`task${taskToAdd.id}`;
+    tr.id = `task${taskToAdd.id}`;
     tr.innerHTML = newRow;
     taskTable.appendChild(tr);
 
@@ -81,10 +88,23 @@ function addTaskToTable(taskToAdd) {
     document.getElementById('id').value = currentTaskId;
 }
 
-function deleteTask(idToDelete){ 
+function addTaskToLocalStore() {
+    localStorage.setItem(TASK_ARRAY_KEY, JSON.stringify(arrayTask));
+}
+
+function deleteTaskFromLocalStore(idTaskToDelete) {
+    console.log(idTaskToDelete);
+    let tempArray = JSON.parse(localStorage.getItem(TASK_ARRAY_KEY));
+    tempArray = tempArray.filter(task => task.id != idTaskToDelete);
+
+    localStorage.setItem(TASK_ARRAY_KEY, JSON.stringify(tempArray));
+}
+
+function deleteTask(idToDelete) {
     arrayTask = arrayTask.filter(task => task.id != idToDelete);
     let rowToDelete = document.getElementById(`task${idToDelete}`);
     taskTable.removeChild(rowToDelete);
+    deleteTaskFromLocalStore(idToDelete);
 }
 
 
